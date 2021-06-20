9249cc087bff7e7d4a0ea98708c35d098d0d0d5d
abhishekasharma206 <70856117+abhishekasharma206@users.noreply.github.com>
2021-06-20 18:18:46 +0530

    RaspberryPi folder for code to bes used on Spyder for Python

---
+++ RaspberyyPi/Rasp_Pi.py
@@ -0,0 +1,37 @@
+#  * - means doubt regarding it. Might work, might not. Need practical testing
+
+import serial
+import RPi.GPIO as GPIO
+import time
+
+ser=serial.Serial("/dev/ttyACM0",9600)
+ser.baudrate=9600
+#defining all the input varaiables
+float ah = 7.5
+float totv #total voltage of the battery pack (*)
+float batcapideal = 94.5
+float batcappract
+float soh
+
+def read_Arduinoinputs():
+	#Write code here
+
+def send_outputsArduino():
+	#Write code here
+
+while True:
+        #Get inputs from Arduino
+	read_Arduinoinputs()
+	
+	#Battery capacity calculation
+	if totv == 12.6:
+		time.sleep(60)
+		read_Arduinoinputs()
+		batcappract = ah * totv
+		soh = batcappract / 94.5  #to get proper decimal output for SoH, we use the denominator a floating point instead of var
+		send_outputsArduino()
+	
+	#Send values to Arduino
+	send_outputsArduino()
+
+
