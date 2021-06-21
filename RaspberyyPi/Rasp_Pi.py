f15bb5fdb6921821472143188ed39c8eac9cacf1
abhishekasharma206 <70856117+abhishekasharma206@users.noreply.github.com>
2021-06-21 19:16:30 +0530

    First 5 comm codes python code for raspberry pi

--- RaspberyyPi/Rasp_Pi.py
+++ RaspberyyPi/Rasp_Pi.py
@@ -9,9 +9,47 @@ ser.baudrate=9600
 #defining all the input varaiables
 ah = 7.5
 batcapideal = 94.5
+series_cell = 3
+parallel_cell = 3
+totalcell = series_cell * parallel_cell
 
 def read_Arduinoinputs():
 	#Write code here
+    if (ser.in_waiting > 0):
+        comm_code = ser.readline()
+        if comm_code == 1:
+            read_cellvolts()
+        elif comm_code == 2:
+            read_totalvolts()
+        elif comm_code == 3:
+            read_cellamps()
+        elif comm_code == 4:
+            read_totalamps()
+        elif comm_code == 5:
+            read_temperature()
+
+def read_cellvolts():
+    for i in series_cell:
+        ser.write('1')
+        par_v[i+1] = ser.readline()
+
+def read_totalvolts():
+    ser.write('2')
+    totv = ser.readline()
+    
+def read_cellamps():
+    for i in totalcell:
+        ser.write('3')
+        cur_cell[i+1] = ser.readline()
+        
+def read_totalamps():
+    ser.write('4')
+    totamp = ser.readline()
+    
+def read_temperature():
+    for i in totalcell:
+        ser.write('5')
+        temp_cell[i+1] = ser.readline()
 
 def send_outputsArduino():
 	#Write code here
