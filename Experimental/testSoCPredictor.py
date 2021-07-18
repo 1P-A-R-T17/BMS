db76c8a4c4c46cc587100fe8e40070fe3e044e1e
Rachit Raj <rachiraj@gmail.com>
2021-07-18 20:38:13 +0530

    Updated as per new prediction model

--- Experimental/testSoCPredictor.py
+++ Experimental/testSoCPredictor.py
@@ -9,11 +9,11 @@ import tensorflow as tf
 from tensorflow import keras
 #print(tf.version)
 
-model = keras.models.load_model('C:\\Users\\rachi\\Desktop\\soc_predictor.h5')
+model = keras.models.load_model('C:\\Users\\rachi\\Desktop\\soc_predictor_new.h5')
 
-current = 3
-temperature = 15
-voltage = 4
+current = 0
+temperature = 0
+voltage = 0
 
 input = [[current, temperature, voltage]]
 
